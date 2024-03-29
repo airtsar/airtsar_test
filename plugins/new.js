687eeff1785a20fd99928a49f6befdc164ad9eec
airtsar <161482851+airtsar@users.noreply.github.com>
2024-03-29 22:23:51 +0700

    Update new.js

--- plugins/new.js
+++ plugins/new.js
@@ -1501,7 +1501,7 @@ Lampa.SettingsApi.addComponent({
 					}
 		});
 /* Телеканалы */
-		Lampa.SettingsApi.addParam({
+		/*Lampa.SettingsApi.addParam({
 					component: 'add_plugin',
 					param: {
 						name: 'add_tv_plugin',
@@ -1691,7 +1691,7 @@ Lampa.SettingsApi.addComponent({
 							name: 'Radio'
 						});
 					}
-		});
+		});*/
 /* Радио */
 		/*Lampa.SettingsApi.addParam({
 					component: 'add_plugin',
@@ -1920,43 +1920,8 @@ Lampa.SettingsApi.addComponent({
                                       });
                                    }
 		}); */
-/* Реклама */
-Lampa.SettingsApi.addParam({
-					component: 'add_plugin',
-					param: {
-						name: 'add_ads',
-      		                        	type: 'title'
-                        		},
-					field: {
-                                name: ads
-                        },
-              onRender: function (item) {
-                setTimeout(function() {
-           //   $('.settings-param-title').insertBefore($('.settings-param').first())
-                $('.settings-param-title').insertAfter($('.settings-param').last())
-                },0);
-                }
 	
-         });	
 /* Счётчик Яндекса */    
-	(function(m, e, t, r, i, k, a) {
-                       m[i] = m[i] || function() {
-                               (m[i].a = m[i].a || []).push(arguments)
-                       };
-                       m[i].l = 1 * new Date();
-                       for(var j = 0; j < document.scripts.length; j++) {
-                               if(document.scripts[j].src === r) {
-                                       return;
-                               }
-                       }
-                       k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-    })
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-    ym(93937344, "init", {clickmap: true,trackLinks: true,accurateTrackBounce: true})
-    var METRIKA = '<noscript><div><img src="https://mc.yandex.ru/watch/93937344" style="position:absolute; left:-9999px;" alt="" /></div></noscript>';
-    $('body').append(METRIKA);
-} // /* addonStart */
-if (!!window.appready) addonStart();
-else Lampa.Listener.follow('app', function(e){if (e.type === 'ready') addonStart()});	
+	
 	
 })();
